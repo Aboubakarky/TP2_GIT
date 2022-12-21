a1cdd20c93d2c50fec096cbab1270f5b96cbdb90
aboubakar <aky2@myges.fr>
2022-12-21 13:07:59 +0100

    Ajout de la fonction factorielle dans la branche 'feature'

--- script.py
+++ script.py
@@ -1,1 +1,13 @@
 print('Bonjour M ! Merci pour ce TP très enrichissant')
+
+# script permettant de calculer le factorielle d'un nombre 
+
+nbr = int(input('Entrez un nombre : '))
+
+def facto():
+    fact = 1
+    for i in range(1, nbr+1):
+      fact = fact * i
+    print (nbr,'! = ',fact)
+
+facto()
